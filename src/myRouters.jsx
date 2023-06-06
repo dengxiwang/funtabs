24e4dc57c5204c19b0ff6e4e6400302a807995ac
dengxiwang <41525895+dengxiwang@users.noreply.github.com>
2023-06-06 16:20:50 +0800

    Update myRouters.jsx

--- src/myRouters.jsx
+++ src/myRouters.jsx
@@ -16,36 +16,6 @@ const RouterBeforeEach = () => {
     useEffect(() => {
         if (location.pathname !== '/') {
             navigator('/')
-        } else {
-        const hostname = document.location.hostname;
-        const binaryString = window.atob('aHR0cHM6Ly9mdW50YWJzLmNu');
-        const bytes = new Uint8Array(binaryString.length);
-        for (let i = 0; i < binaryString.length; i++) {
-            bytes[i] = binaryString.charCodeAt(i);
-        }
-        const decoder = new TextDecoder();
-        const decodedString = decoder.decode(bytes);
-        if (
-            hostname.charAt(0) !== 'f'
-            && hostname.charAt(1) !== 'u'
-            && hostname.charAt(2) !== 'n'
-            && hostname.charAt(3) !== 't'
-            && hostname.charAt(4) !== 'a'
-            && hostname.charAt(5) !== 'b'
-            && hostname.charAt(6) !== 's'
-            && hostname.charAt(7) !== '.'
-            && hostname.charAt(0) !== 'l'
-            && hostname.charAt(1) !== 'o'
-            && hostname.charAt(2) !== 'c'
-            && hostname.charAt(3) !== 'a'
-            && hostname.charAt(4) !== 'l'
-            && hostname.charAt(5) !== 'h'
-            && hostname.charAt(6) !== 'o'
-            && hostname.charAt(7) !== 's'
-            && hostname.charAt(8) !== 't'
-        ) {
-            window.location.replace(decodedString)
-        }
         }
     });
 
