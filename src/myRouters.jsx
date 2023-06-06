a465612f1d57e9f09198e8ffdcbefff6ae9ba80b
dengxiwang <dengxiwang@vip.qq.com>
2023-06-06 16:29:06 +0800

    更新依赖包

--- src/myRouters.jsx
+++ src/myRouters.jsx
@@ -1,7 +1,7 @@
 // 从网上抄来的react路由守卫
 import { Spin } from "antd";
-import { Suspense, useEffect } from "react";
-import { Outlet, useLocation, useNavigate, useRoutes } from "react-router-dom";
+import { Suspense } from "react";
+import { Outlet, useRoutes } from "react-router-dom";
 import { Home } from "./module/home";
 const Myrouters = [
     {
@@ -11,44 +11,6 @@ const Myrouters = [
 ];
 
 const RouterBeforeEach = () => {
-    const location = useLocation();
-    const navigator = useNavigate()
-    useEffect(() => {
-        if (location.pathname !== '/') {
-            navigator('/')
-        } else {
-        const hostname = document.location.hostname;
-        const binaryString = window.atob('aHR0cHM6Ly9mdW50YWJzLmNu');
-        const bytes = new Uint8Array(binaryString.length);
-        for (let i = 0; i < binaryString.length; i++) {
-            bytes[i] = binaryString.charCodeAt(i);
-        }
-        const decoder = new TextDecoder();
-        const decodedString = decoder.decode(bytes);
-        if (
-            hostname.charAt(0) !== 'f'
-            && hostname.charAt(1) !== 'u'
-            && hostname.charAt(2) !== 'n'
-            && hostname.charAt(3) !== 't'
-            && hostname.charAt(4) !== 'a'
-            && hostname.charAt(5) !== 'b'
-            && hostname.charAt(6) !== 's'
-            && hostname.charAt(7) !== '.'
-            && hostname.charAt(0) !== 'l'
-            && hostname.charAt(1) !== 'o'
-            && hostname.charAt(2) !== 'c'
-            && hostname.charAt(3) !== 'a'
-            && hostname.charAt(4) !== 'l'
-            && hostname.charAt(5) !== 'h'
-            && hostname.charAt(6) !== 'o'
-            && hostname.charAt(7) !== 's'
-            && hostname.charAt(8) !== 't'
-        ) {
-            window.location.replace(decodedString)
-        }
-        }
-    });
-
     return (<>
         <Outlet />
     </>)
